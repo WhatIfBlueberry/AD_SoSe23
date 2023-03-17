312a853c0ddae8692b7fe7dad74902a6d98dff11
Dylan Rau <dylan-pascal@web.de>
2023-03-17 18:23:29 +0100

    answers.txt, new workbench

--- ex1/workbench.py
+++ ex1/workbench.py
@@ -2,8 +2,11 @@ from euclidean import euclidean
 from euclideanRecursive import euclideanRecursive
 from kgV import kgV
 
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-print("Euclidean: ", euclidean(x, y))
-print("Euclidean recursive: ",euclideanRecursive(x, y))
-print("kgV: ", kgV(x, y))
+numbers = [x for x in range(30, 41)]
+numbersY = [x for x in range(40, 29, -1)]
+# call euclidean and kgV for all numbers
+for x in numbers:
+    for y in numbersY:
+        print("ggT: ", euclidean(x, y))
+        print("kgV: ", kgV(x, y))
+        print("a * b: ", x * y)
